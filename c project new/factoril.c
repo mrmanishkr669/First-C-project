bf14b99b853351a9f5398234a93987b7df5f54b4
MANISH KUMAR <7370manishkumar@gmail.com>
2024-10-04 22:12:36 +0530

    factorial

---
+++ c project new/factoril.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int factorial(int n);
+
+int main(){
+    int n;
+    printf("enter n :    ");
+    scanf("%d", &n);
+    printf("Factorial is : %d", factorial(n));
+    return 0;
+
+}
+int factorial(int n){
+    if(n==0){
+        return 1;
+    }
+    int factnm1 = factorial(n-1);
+    int factn = factnm1*n;
+    return factn;
+}
+
+
+
+
+
+
