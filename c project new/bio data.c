fa312244a8e60b7212902604a9d00baeb349ae28
MANISH KUMAR <7370manishkumar@gmail.com>
2024-10-18 22:43:24 +0530

    Resume

---
+++ c project new/bio data.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    printf("\t\t\t\t\t\t\tBio Data");
+    printf("\n\t\tName :-\t\t\t\tManish Kumar");
+    printf("\n\t\tContact :-\t\t\t7370842348");
+    printf("\n\t\tEmail :-\t\t\t&7370manishkumar@gmail.com");
+    printf("\n\t\tDate of birth :\t\t\t01-01-2004");
+    printf("\n\t\tGender :\t\t\tMale");
+    printf("\n\t\tFather Name :\t\t\tNagendra Singh");
+    printf("\n\t\tReligion :\t\t\tHindu ");
+    printf("\n\t\tNationality :\t\t\tIndian");
+    printf("\n\t\tMarital status :-\t\tUnmarried");
+    printf("\n\t\tLanguage known:-\t\t Hindi , English");
+    printf("\n\t\tEducation :-\t\t\tBCA");
+    printf("\n\t\tAddress :-\t\t\tVillage+Post = Bhitti, PS = Mohania , Dist = Kaimur (Bhabua) Bihar- 821109");
+}
+    
